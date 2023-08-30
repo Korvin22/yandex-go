d898636c83f5ffae7c2c9f2830217dc9a9f73be4
Dmitrii Gudemenko <dmitry.gudemenko@yandex.ru>
2023-08-30 07:17:01 +0300

    infrastructure

---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from './App.module.css';
+
+
+function App() {
+
+  return (
+    <div className={styles.app}>
+      <p>1</p>
+    </div>
+  );
+}
+
+export default App;
